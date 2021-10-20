d7eb17ebba2d541a1222927232ffc458a8df4fdc
experiencei <ayelojahighbee01@gmail.com>
2021-10-20 16:39:29 +0100

    using react tiker and firebase for db to clone tiktok

---
+++ src/components/firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC1r2wDrPXuDyimNWQft0-K0D25YlWzVxo",
+    authDomain: "tiktokclone-5ebec.firebaseapp.com",
+    projectId: "tiktokclone-5ebec",
+    storageBucket: "tiktokclone-5ebec.appspot.com",
+    messagingSenderId: "749713008292",
+    appId: "1:749713008292:web:7d0f8458fd1adada79e831"
+  };
