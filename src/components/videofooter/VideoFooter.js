8a2fb58f124650b405664621557b9bfa2ec8d261
experiencei <ayelojahighbee01@gmail.com>
2021-10-20 17:52:54 +0100

    working on the video footer

--- src/components/videofooter/VideoFooter.js
+++ src/components/videofooter/VideoFooter.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import "./VideoFooter.css"
+import "./VideoFooter.css";
+
 function VideoFooter() {
     return (
         <div className="videoFooter">
             <div className="videoFooter__text">
             </div>
-            
+
             <img
             className="videoFooter__record"
             src="https://static.thenounproject.com/png/934821-200.png"
