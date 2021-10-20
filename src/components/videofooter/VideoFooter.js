490071a9fe4343faade9bc7b42cf21b444234b08
experiencei <ayelojahighbee01@gmail.com>
2021-10-20 17:50:54 +0100

    working on the video footer

--- src/components/videofooter/VideoFooter.js
+++ src/components/videofooter/VideoFooter.js
@@ -3,7 +3,14 @@ import "./VideoFooter.css"
 function VideoFooter() {
     return (
         <div className="videoFooter">
-            <h2></h2>
+            <div className="videoFooter__text">
+            </div>
+            
+            <img
+            className="videoFooter__record"
+            src="https://static.thenounproject.com/png/934821-200.png"
+            alt=""
+      />
         </div>
     )
 }
